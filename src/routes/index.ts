d3424885a32268c4fdf47312720a160b7d841dc5
fixruan <ruangoio01@gmail.com>
2022-12-14 19:09:09 -0300

    Create Movie Route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import {Router} from "express"
+import {movieRoutes} from "./movie.route";
 import {userRoutes} from "./user.route";
 
 const routes = Router();
 
 routes.use("/users", userRoutes);
+routes.use("/movies", movieRoutes);
 
 export {routes};
